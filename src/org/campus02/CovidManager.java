36906fa07c50ec038ecfa581818eb61e85589ade
skochauf <stephan.kochauf@lv.campus02.at>
2021-11-12 20:52:22 +0100

    CovidManager mit Testfällen

---
+++ src/org/campus02/CovidManager.java
@@ -0,0 +1,16 @@
+package org.campus02;
+
+public class CovidManager {
+
+    public static void main(String[] args) {
+
+        Incidence stmk20211111 = new Incidence(
+                "Steiermark", "2021-11-11", 1300);
+        Incidence stmk20211110 = new Incidence(
+                "Steiermark", "2021-11-10", 1200);
+
+
+
+
+    }
+}
