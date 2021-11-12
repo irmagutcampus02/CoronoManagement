320d109abe9b237c7f0cd0e39187631f674ceac4
skochauf <stephan.kochauf@lv.campus02.at>
2021-11-12 21:31:08 +0100

    Showcase ArrayList & HashMap

--- src/org/campus02/CovidManager.java
+++ src/org/campus02/CovidManager.java
@@ -1,5 +1,8 @@
 package org.campus02;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CovidManager {
 
     public static void main(String[] args) {
@@ -8,9 +11,69 @@ public class CovidManager {
                 "Steiermark", "2021-11-11", 1300);
         Incidence stmk20211110 = new Incidence(
                 "Steiermark", "2021-11-10", 1200);
+        Incidence wien20211111 = new Incidence(
+                "Wien", "2021-11-11", 5631);
+        Incidence wien20211110 = new Incidence(
+                "Wien", "2021-11-10", 4832);
+        Incidence tirol20211111 = new Incidence(
+                "Tirol", "2021-11-11", 999);
+
+        ArrayList<Incidence> recordedValues = new ArrayList<>();
+        recordedValues.add(stmk20211110); // STRG+D
+        recordedValues.add(stmk20211111); // STRG+D
+        recordedValues.add(wien20211110); // STRG+D
+        recordedValues.add(wien20211111); // STRG+D
+        recordedValues.add(tirol20211111); // STRG+D
+        recordedValues.add(new Incidence("Salzburg",
+                "2021-11-11", 2999));
+
+        System.out.println("Count of recorded Records: " + recordedValues.size());
+
+        // Count der gesamten Fälle
+        int countTotalIncidences = 0;
+        for (int i = 0; i < recordedValues.size(); i++) {
+            Incidence oneValue = recordedValues.get(i);
+            //countTotalIncidences += oneValue.getNumber();
+            countTotalIncidences = countTotalIncidences + oneValue.getNumber();
+        }
+        System.out.println("Bisher in Österreich: " + countTotalIncidences + " Fälle");
+
+        countTotalIncidences = 0;
+        for (Incidence incidence : recordedValues) {
+            // für jeden Incidence in recordedValues
+            countTotalIncidences += incidence.getNumber();
+        }
+        System.out.println("Bisher in Österreich: " + countTotalIncidences + " Fälle");
+
+        int countTotalIncidencesStmk = 0;
+        // iter
+        for (Incidence recordedValue : recordedValues) {
+            if (recordedValue.getState().equals("Steiermark"))
+                countTotalIncidencesStmk += recordedValue.getNumber();
+        }
+        System.out.println("countTotalIncidencesStmk = " + countTotalIncidencesStmk);
 
 
-
-
+        // Anzahl der Fälle je Bundesland
+        HashMap<String, Integer> totalIncidencesState = new HashMap<>();
+        for (Incidence oneElement : recordedValues) {
+            /*
+                1. Ist mein Key bereits in der Liste
+                    2. wenn nein, dann muss ich den Key aufnehmen
+                    3. wenn ja, dann muss ich den Wert auslesen, summieren, neu ablegen
+             */
+            if (totalIncidencesState.containsKey(oneElement.getState())){
+                // ja, Fall 3
+                int currentValue = totalIncidencesState.get(oneElement.getState());
+                currentValue += oneElement.getNumber();
+                totalIncidencesState.put(oneElement.getState(), currentValue);
+            }
+            else {
+                // nein, Fall 2
+                totalIncidencesState.put(oneElement.getState(),
+                        oneElement.getNumber());
+            }
+        }
+        System.out.println("totalIncidencesState = " + totalIncidencesState);
     }
 }
